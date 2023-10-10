c92d39ba4a8abebeadc462a2a236e9d5e84b8b5d
Aidan Pine <hello@aidanpine.ca>
2023-10-10 19:31:28 +0000

    refactor: make noam scheduler linked to step count

--- fs2/noam.py
+++ fs2/noam.py
@@ -18,8 +18,12 @@ class NoamLR(_LRScheduler):
         super().__init__(optimizer)
 
     def get_lr(self):
-        last_epoch = max(1, self.last_epoch)
-        scale = self.warmup_steps**0.5 * min(
-            last_epoch ** (-0.5), last_epoch * self.warmup_steps ** (-1.5)
-        )
+        # Protect against raising 0 to negative power
+        if self.warmup_steps == 0:
+            scale = 1.0
+        # After warmup scale according to the step count
+        elif self._step_count > self.warmup_steps:
+            scale = self._step_count**-0.5
+        else:
+            scale = self._step_count * (self.warmup_steps**-1.5)
         return [base_lr * scale for base_lr in self.base_lrs]
