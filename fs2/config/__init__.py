5a8ba3cbb068794982f373055f4aa30fd6cc2bb8
Eric Joanis <Eric.Joanis@nrc-cnrc.gc.ca>
2025-10-22 13:24:00 -0400

    fix: correct a bunch of pyright-raised annotation errors

--- fs2/config/__init__.py
+++ fs2/config/__init__.py
@@ -29,22 +29,23 @@ LATEST_VERSION: str = "1.1"
 
 
 class ConformerConfig(ConfigModel):
-    layers: int = Field(4, description="The number of layers in the Conformer.")
+    layers: int = Field(default=4, description="The number of layers in the Conformer.")
     heads: int = Field(
-        2, description="The number of heads in the multi-headed attention modules."
+        default=2,
+        description="The number of heads in the multi-headed attention modules.",
     )
     input_dim: int = Field(
-        256,
+        default=256,
         description="The number of hidden dimensions in the input. The input_dim value declared in the encoder and decoder modules must match the input_dim value declared in each variance predictor module.",
     )
     feedforward_dim: int = Field(
-        1024, description="The number of dimensions in the feedforward layers."
+        default=1024, description="The number of dimensions in the feedforward layers."
     )
     conv_kernel_size: int = Field(
-        9,
+        default=9,
         description="The size of the kernel in each convoluational layer of the Conformer.",
     )
-    dropout: float = Field(0.2, description="The amount of dropout to apply.")
+    dropout: float = Field(default=0.2, description="The amount of dropout to apply.")
 
 
 class FastSpeech2Variances(ConfigModel):
@@ -65,26 +66,27 @@ class VarianceLossEnum(str, Enum):
 
 class VariancePredictorBase(ConfigModel):
     loss: VarianceLossEnum = Field(
-        VarianceLossEnum.mse,
+        default=VarianceLossEnum.mse,
         description="The loss function to use when calculate variance loss. Either 'mse' or 'mae'.",
     )
     n_layers: int = Field(
-        5, description="The number of layers in the variance predictor module."
+        default=5, description="The number of layers in the variance predictor module."
     )
     kernel_size: int = Field(
-        3,
+        default=3,
         description="The kernel size of each convolutional layer in the variance predictor module.",
     )
-    dropout: float = Field(0.5, description="The amount of dropout to apply.")
+    dropout: float = Field(default=0.5, description="The amount of dropout to apply.")
     input_dim: int = Field(
-        256,
+        default=256,
         description="The number of hidden dimensions in the input. This must match the input_dim value declared in the encoder and decoder modules.",
     )
     n_bins: int = Field(
-        256, description="The number of bins to use in the variance predictor module."
+        default=256,
+        description="The number of bins to use in the variance predictor module.",
     )
     depthwise: bool = Field(
-        True, description="Whether to use depthwise separable convolutions."
+        default=True, description="Whether to use depthwise separable convolutions."
     )
 
     @field_serializer("loss")
@@ -94,7 +96,7 @@ class VariancePredictorBase(ConfigModel):
 
 class VariancePredictorConfig(VariancePredictorBase):
     level: VarianceLevelEnum = Field(
-        VarianceLevelEnum.phone,
+        default=VarianceLevelEnum.phone,
         description="The level for the variance predictor to use. 'frame' will make predictions at the frame level. 'phone' will average predictions across all frames in each phone.",
     )
 
@@ -135,27 +137,30 @@ class FastSpeech2ModelConfig(ConfigModel):
         TargetTrainingTextRepresentationLevel.characters
     )
     learn_alignment: bool = Field(
-        True,
+        default=True,
         description="Advanced. Whether to jointly learn alignments using monotonic alignment search module (See Badlani et. al. 2021: https://arxiv.org/abs/2108.10447). If set to False, you will have to provide text/audio alignments separately in 'preprocessed/duration' before training a text-to-spec (feature prediction) model.",
     )
     use_global_style_token_module: bool = Field(
-        False,
+        default=False,
         description="Whether to use the Global Style Token (GST) module from Style Tokens: Unsupervised Style Modeling, Control and Transfer in End-to-End Speech Synthesis (https://arxiv.org/abs/1803.09017)",
     )
     max_length: int = Field(
-        1000, description="The maximum length (i.e. number of symbols) for text inputs."
+        default=1000,
+        description="The maximum length (i.e. number of symbols) for text inputs.",
     )
     mel_loss: VarianceLossEnum = Field(
-        VarianceLossEnum.mse,
+        default=VarianceLossEnum.mse,
         description="The loss function to use when calculating Mel spectrogram loss.",
     )
-    use_postnet: bool = Field(True, description="Whether to use a postnet module.")
+    use_postnet: bool = Field(
+        default=True, description="Whether to use a postnet module."
+    )
     multilingual: bool = Field(
-        False,
+        default=False,
         description="Whether to train a multilingual model. For this to work, your filelist must contain a column/field for 'language' with values for each utterance.",
     )
     multispeaker: bool = Field(
-        False,
+        default=False,
         description="Whether to train a multispeaker model. For this to work, your filelist must contain a column/field for 'speaker' with values for each utterance.",
     )
 
@@ -187,11 +192,11 @@ class EarlyStoppingConfig(ConfigModel):
 
 class FastSpeech2TrainingConfig(BaseTrainingConfig):
     use_weighted_sampler: bool = Field(
-        False,
+        default=False,
         description="Whether to use a sampler which oversamples from the minority language or speaker class for balanced training.",
     )
     optimizer: NoamOptimizer = Field(
-        NoamOptimizer(
+        default=NoamOptimizer(
             betas=(0.9, 0.999), learning_rate=1e-3, warmup_steps=1000, weight_decay=1e-6
         ),
         description="The optimizer to use during training.",
@@ -200,28 +205,29 @@ class FastSpeech2TrainingConfig(BaseTrainingConfig):
     # early_stopping: EarlyStoppingConfig = Field(default_factory=EarlyStoppingConfig)
     vocoder_path: Union[FilePath, None] = None
     mel_loss_weight: float = Field(
-        1.0, description="Multiply the spec loss by this weight"
+        default=1.0, description="Multiply the spec loss by this weight"
     )
     postnet_loss_weight: float = Field(
-        1.0, description="Multiply the postnet loss by this weight"
+        default=1.0, description="Multiply the postnet loss by this weight"
     )
     pitch_loss_weight: float = Field(
-        0.1, description="Multiply the pitch loss by this weight"
+        default=0.1, description="Multiply the pitch loss by this weight"
     )
     energy_loss_weight: float = Field(
-        0.1, description="Multiply the energy loss by this weight"
+        default=0.1, description="Multiply the energy loss by this weight"
     )
     duration_loss_weight: float = Field(
-        0.1, description="Multiply the duration loss by this weight"
+        default=0.1, description="Multiply the duration loss by this weight"
     )
     attn_ctc_loss_weight: float = Field(
-        0.1, description="Multiply the Attention CTC loss by this weight"
+        default=0.1, description="Multiply the Attention CTC loss by this weight"
     )
     attn_bin_loss_weight: float = Field(
-        0.1, description="Multiply the Attention Binarization loss by this weight"
+        default=0.1,
+        description="Multiply the Attention Binarization loss by this weight",
     )
     attn_bin_loss_warmup_epochs: Annotated[int, Ge(1)] = Field(
-        100,
+        default=100,
         description="Scale the Attention Binarization loss by (current_epoch / attn_bin_loss_warmup_epochs) until the number of epochs defined by attn_bin_loss_warmup_epochs is reached.",
     )
 
@@ -248,7 +254,7 @@ class FastSpeech2Config(BaseModelWithContact):
         description="The model configuration settings.",
     )
     path_to_model_config_file: Optional[FilePath] = Field(
-        None, description="The path of a model configuration file."
+        default=None, description="The path of a model configuration file."
     )
 
     training: FastSpeech2TrainingConfig = Field(
@@ -256,7 +262,7 @@ class FastSpeech2Config(BaseModelWithContact):
         description="The training configuration hyperparameters.",
     )
     path_to_training_config_file: Optional[FilePath] = Field(
-        None, description="The path of a training configuration file."
+        default=None, description="The path of a training configuration file."
     )
 
     preprocessing: PreprocessingConfig = Field(
@@ -264,14 +270,14 @@ class FastSpeech2Config(BaseModelWithContact):
         description="The preprocessing configuration, including information about audio settings.",
     )
     path_to_preprocessing_config_file: Optional[FilePath] = Field(
-        None, description="The path of a preprocessing configuration file."
+        default=None, description="The path of a preprocessing configuration file."
     )
 
     text: TextConfig = Field(
         default_factory=TextConfig, description="The text configuration."
     )
     path_to_text_config_file: Optional[FilePath] = Field(
-        None, description="The path of a text configuration file."
+        default=None, description="The path of a text configuration file."
     )
 
     @model_validator(mode="before")  # type: ignore
