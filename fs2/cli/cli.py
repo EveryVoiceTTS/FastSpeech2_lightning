5e5d2ad0aa7f89b6a38f44256897c4de56bc455d
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-08-27 11:31:16 -0400

    docs: ev preprocess command now needs a subcommand

--- fs2/cli/cli.py
+++ fs2/cli/cli.py
@@ -29,11 +29,11 @@ command(
 
     By default every step of the preprocessor will be done by running:
     \n\n
-    **everyvoice preprocess config/{TEXT_TO_SPEC_CONFIG_FILENAME_PREFIX}.yaml**
+    **everyvoice preprocess text-to-spec config/{TEXT_TO_SPEC_CONFIG_FILENAME_PREFIX}.yaml**
     \n\n
     If you only want to process specific things, you can run specific commands by adding them as options for example:
     \n\n
-    **everyvoice preprocess config/{TEXT_TO_SPEC_CONFIG_FILENAME_PREFIX}.yaml -s energy -s pitch**
+    **everyvoice preprocess text-to-spec config/{TEXT_TO_SPEC_CONFIG_FILENAME_PREFIX}.yaml -s energy -s pitch**
     """,
 )(app_preprocess)
 
